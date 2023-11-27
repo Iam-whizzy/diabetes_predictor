b7b2cd0cb5d89c2943231500f478ebb4a6802d9b
Whizzy <wiztyping@gmail.com>
2023-11-28 00:50:34 +0300

    First diabetes_predictor code commit

---
+++ lib/main.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyForm(),
+    );
+  }
+}
+
+class MyForm extends StatefulWidget {
+  @override
+  _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  final GlobalKey<FormBuilderState> _formKey = GlobalKey<FormBuilderState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Diabetes Predictor'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(
+            children: [
+              FormBuilderTextField(
+                name: 'children',
+                decoration: InputDecoration(labelText: 'Children'),
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.numeric(context),
+              ),
+              FormBuilderTextField(
+                name: 'glucose',
+                decoration: InputDecoration(labelText: 'Glucose'),
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.numeric(context),
+              ),
+              FormBuilderTextField(
+                name: 'bloodPressure',
+                decoration: InputDecoration(labelText: 'Blood Pressure'),
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.numeric(context),
+              ),
+              FormBuilderTextField(
+                name: 'skinThickness',
+                decoration: InputDecoration(labelText: 'Skin Thickness'),
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.numeric(context),
+              ),
+              FormBuilderTextField(
+                name: 'insulin',
+                decoration: InputDecoration(labelText: 'Insulin'),
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.numeric(context),
+              ),
+              FormBuilderTextField(
+                name: 'bmi',
+                decoration: InputDecoration(labelText: 'BMI'),
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.numeric(context),
+              ),
+              FormBuilderTextField(
+                name: 'diabetesPedigreeFunction',
+                decoration:
+                    InputDecoration(labelText: 'Diabetes Pedigree Function'),
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.numeric(context),
+              ),
+              FormBuilderTextField(
+                name: 'age',
+                decoration: InputDecoration(labelText: 'Age'),
+                keyboardType: TextInputType.number,
+                validator: FormBuilderValidators.numeric(context),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.saveAndValidate()) {
+                    // Access the form data
+                    Map<String, dynamic> formData =
+                        _formKey.currentState!.value;
+                    // Use the form data as needed, e.g., pass it to your ML model
+                    print(formData);
+                  }
+                },
+                child: Text('Predict'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:diabetes_predictor/main.dart';
+
+void main() {
+  testWidgets('Diabetes Predictor Form Test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Fill in some data in the form fields.
+    await tester.enterText(find.byKey(const Key('children')), '2');
+    await tester.enterText(find.byKey(const Key('glucose')), '120');
+    // Repeat this for other form fields...
+
+    // Tap the 'Predict' button.
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump();
+
+    // Verify that the form data is printed.
+    expect(
+        find.text('{"children": "2", "glucose": "120", ...}'), findsOneWidget);
+  });
+}
